38846792e718e64ca043a9d36233c1e49377c5b1
hiroi_junki <junki.hiroi@drecom.co.jp>
2022-01-16 20:01:25 +0900

    Add BallShot script

---
+++ Assets/Scenes/Scripts/BallShot.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class BallShot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ballPrefab;
+    [SerializeField]
+    private float power;
+
+    [SerializeField]
+    // 発射間のインターバルタイム
+    private float intervalTime;
+    
+    private float _time = 0;
+
+    private const int MouseLeft = 0;
+
+    void Update()
+    {
+        _time -= Time.deltaTime;
+        if (_time < 0)
+        {
+            if (Input.GetMouseButtonDown(MouseLeft))
+            {
+                var ball = Instantiate(ballPrefab);
+                ball.transform.position = transform.position;
+                Debug.Assert(Camera.main != null, "Camera.main != null");
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                var target = ray.direction;
+                ball.GetComponent<Rigidbody>().velocity = target * power;
+                _time = intervalTime;
+            }
+        }
+    }
+}
